17b684d0e848d5406acbad38b9f4e9610fd3df8f
Ricardo <ricardo.dev1991@gmail.com>
2023-11-23 17:50:48 -0300

    Adicionando atividades feitas em sala de aula

---
+++ semana-3/Program.cs
@@ -0,0 +1,10 @@
+﻿var tuple = (nome: "Ricardo", altura: 20);
+var tuple2 = (nome: "Alexandre", altura: 30);
+var tuple3 = (nome: "Paulo", altura: 40);
+
+List<(string nome, int altura)> list = new();
+
+list.Add(tuple);
+list.Add(tuple2);
+list.Add(tuple3);
+
