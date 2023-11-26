60360b6629505ffa3970ba41f234338d45c1c155
ricardo <ricardo.dev1991@gmail.com>
2023-11-26 19:45:26 -0300

    ✨ feat(Adicionando estrutura básica de menu para o projeto):

---
+++ dotnet-P003/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MeuPrimeiroProjeto 
+{
+    class Program {
+        static void Main(string[] args)
+        {
+           string? option;
+        
+           do { 
+                Console.WriteLine("Gerenciamento de Estoque de Produtos");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("1 - Cadastrar um novo Produto");
+                Console.WriteLine("2 - Consultar um produto");
+                Console.WriteLine("3 - Atualizar quantidade do produto");
+                Console.WriteLine("4 - Gerar relatórios");
+        
+                Console.WriteLine("Escolha uma opção:");
+                option = Console.ReadLine();
+                
+                switch (option)
+                {
+                    case "1":
+                  
+                        break;
+                    case "2":
+                  
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+           }while(option != "0");
+        }
+    }
+}
