377b23c12d297f2b27cb5e63b23d1bc972735f4b
ricardo <ricardo.dev1991@gmail.com>
2023-11-26 20:41:45 -0300

    ✨ feat(Implementando método de consultar um produto por código. Filtragem.):

--- dotnet-P003/ProductManager.cs
+++ dotnet-P003/ProductManager.cs
@@ -10,47 +10,64 @@ namespace ProductManagerNamespace
         public List<(int? code, string? name, int? amount, float? price )> productList = new();
 
         public void AddProduct(){
-            float? productPrice = 0;
-            int? productAmount = 0, productCode = null;
-            
-            Console.WriteLine("Digite o nome do produto");
-            string? productName = Console.ReadLine();
+            try {
+                float? productPrice = 0;
+                int? productAmount = 0, productCode = null;
+                
+                Console.WriteLine("Digite o nome do produto");
+                string? productName = Console.ReadLine();
 
-            Console.WriteLine("O preço unitário do produto:");
-            string? inputProductFloat = Console.ReadLine();
-            if(float.TryParse(inputProductFloat, out float price)){
-                productPrice = price;
-            } else {
-                Console.WriteLine("Entrada inválida");
-            }
-            Console.WriteLine("Quantidade do produto:");
-            string? inputProductAmount = Console.ReadLine();
-            if(int.TryParse(inputProductAmount, out int amount)){
-                productAmount = amount;
-            } else {
-                Console.WriteLine("Entrada Inválida");
-            }
-            Console.WriteLine("Código do produto:");
-            string? inputProductCode = Console.ReadLine();
-            if(int.TryParse(inputProductCode, out int code)){
-                productCode = code;
-            } else {
-                Console.WriteLine("Entrada inválida");
-            }
+                Console.WriteLine("O preço unitário do produto:");
+                string? inputProductFloat = Console.ReadLine();
 
-            var newProduct = (code: productCode, name: productName, amount: productAmount, price: productPrice);
-            productList.Add(newProduct);
+                if(float.TryParse(inputProductFloat, out float price)){
+                    productPrice = price;
+                } else {
+                    throw new Exception("Entrada inválida para preço. Deve ser um número válido.");
+                }
+
+                Console.WriteLine("Quantidade do produto:");
+                string? inputProductAmount = Console.ReadLine();
+
+                if(int.TryParse(inputProductAmount, out int amount)){
+                    productAmount = amount;
+                } else {
+                    throw new Exception("Entrada inválida para quantidade do produto. Deve ser um número válido.");
+                }
+
+                Console.WriteLine("Código do produto:");
+                string? inputProductCode = Console.ReadLine();
+
+                if(int.TryParse(inputProductCode, out int code)){
+                    productCode = code;
+                } else {
+                    throw new Exception("Entrada inválida para o código do produto. Deve ser uma entrada válida.");
+                }
+
+                var newProduct = (code: productCode, name: productName, amount: productAmount, price: productPrice);
+                productList.Add(newProduct);
+            }catch(Exception err){
+                Console.WriteLine($"Erro: {err.Message}");
+            }
         }
 
         public void consultProductByCode() {
             int? productCode = null;
-            foreach(var product in productList){
-                Console.WriteLine($"Nome: {product.name}\nPreço: {product.price}\nQuantidade: {product.amount}:\nCódigo: {product.code} ");
-                Console.WriteLine();
-            }
+            try {
+                Console.WriteLine("Digite o código do produto que deseja consultar:");
+                string? inputProductCode = Console.ReadLine();
+                if(int.TryParse(inputProductCode, out int code)){
+                    productCode = code;
+                } else {
+                    throw new Exception("Entrada inválida para código do produto. Deve ser uma entrada válida.");
+                }
+                var filteredProduct = productList.SingleOrDefault(product => product.code == code);
 
-            Console.WriteLine("Digite o código do produto que deseja consultar:");
-            string? inputProductCode = Console.ReadLine();
+                Console.WriteLine($"Nome: {filteredProduct.name}\nPreço: R${filteredProduct.price}\nQuantidade: {filteredProduct.amount}\nCódigo: {filteredProduct.code}");
+
+            }catch(Exception err){
+                Console.WriteLine($"Error: {err.Message}");
+            }
         }
     }
 }
