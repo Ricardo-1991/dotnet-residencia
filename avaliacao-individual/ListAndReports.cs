c1a8e85b6bed478f4aec92eb142609c9935a2d15
ricardo <ricardo.dev1991@gmail.com>
2023-11-30 17:43:59 -0300

    Adicionando método de gerar relatórios.

--- avaliacao-individual/ListAndReports.cs
+++ avaliacao-individual/ListAndReports.cs
@@ -2,14 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Cliente = Personas.Cliente;
-using Advogado = Personas.Advogado;
+using AddPeople = namespaceAddPeople.AddPeople;
 
 namespace Reports
 {
     public static class ListAndReports {
         public static void reports(){
+            Console.WriteLine("Relatórios");
+            int idadeA = 20, idadeB = 30;
+
+            if(idadeA < idadeB){
+                (idadeA, idadeB) = (idadeB, idadeA);
+            }
+            var filteredListValues = AddPeople.colecaoCliente.Select(cliente => cliente.dataNascimento);
             
+            /* foreach(var product in filteredListValues){
+                Console.WriteLine(divLine);
+                Console.WriteLine($"Nome: {product.name}\nPreço: R${product.price}\nQuantidade: {product.amount}\nCódigo: {product.code}");
+                Console.WriteLine(divLine);
+            } */
         }
     }
 }
