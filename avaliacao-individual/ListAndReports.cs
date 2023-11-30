d006be08e7d29523748fdb0696e1a7ce3882fbd8
ricardo <ricardo.dev1991@gmail.com>
2023-11-30 17:58:53 -0300

    Adicionando filtros de idades e por estado civil.

--- avaliacao-individual/ListAndReports.cs
+++ avaliacao-individual/ListAndReports.cs
@@ -10,17 +10,46 @@ namespace Reports
         public static void reports(){
             Console.WriteLine("Relatórios");
             int idadeA = 20, idadeB = 30;
-
+    
             if(idadeA < idadeB){
                 (idadeA, idadeB) = (idadeB, idadeA);
             }
-            var filteredListValues = AddPeople.colecaoCliente.Select(cliente => cliente.dataNascimento);
-            
-            /* foreach(var product in filteredListValues){
-                Console.WriteLine(divLine);
-                Console.WriteLine($"Nome: {product.name}\nPreço: R${product.price}\nQuantidade: {product.amount}\nCódigo: {product.code}");
-                Console.WriteLine(divLine);
-            } */
+            var filteredListCliente = AddPeople.colecaoCliente.Where(cliente => DateTime.Now.Year - cliente.dataNascimento.Year >= idadeA && DateTime.Now.Year - cliente.dataNascimento.Year <= idadeB);
+            foreach(var cliente in filteredListCliente){
+                Console.WriteLine($"Nome: {cliente.nome} - Data de Nascimento: {cliente.dataNascimento}");
+            }
+
+            var filteredListAdvogado = AddPeople.colecaoAdvogado.Where(advogado => DateTime.Now.Year - advogado.dataNascimento.Year >= idadeA && DateTime.Now.Year - advogado.dataNascimento.Year <= idadeB);
+            Console.WriteLine("Clientes");
+            foreach(var cliente in filteredListCliente){
+                Console.WriteLine($"Nome: {cliente.nome} - Data de Nascimento: {cliente.dataNascimento}");
+            }
+
+            Console.WriteLine("Advogados");
+            foreach(var advogado in filteredListAdvogado){
+                Console.WriteLine($"Nome: {advogado.nome} - Data de Nascimento: {advogado.dataNascimento}");
+            }
+
+            Console.WriteLine("Estado Civil de CLientes");
+            string estadoCivil = "Solteiro";
+
+            var filteredListEstadoCivilCliente = AddPeople.colecaoCliente.Where(advogado => advogado.estadoCivil == estadoCivil.ToLower());
+            foreach(var cliente in filteredListEstadoCivilCliente){
+                Console.WriteLine($"Nome: {cliente.nome} - Data de Nascimento: {cliente.dataNascimento}");
+            }
         }
+
+        public static int CalculateAge(DateTime dataNascimento)
+{
+    int idade = DateTime.Now.Year - dataNascimento.Year;
+
+    // Ajuste se a data de nascimento ainda não ocorreu este ano
+    if (DateTime.Now < dataNascimento.AddYears(idade))
+    {
+        idade--;
+    }
+
+    return idade;
+}
     }
 }
--- avaliacao-individual/Program.cs
+++ avaliacao-individual/Program.cs
@@ -7,9 +7,9 @@ using addPeople = namespaceAddPeople.AddPeople;
 Advogado advogado1 = new Advogado("Dorea", new DateTime(1989, 7, 24));
 Advogado advogado2 = new Advogado("Alexandre", new DateTime(2000, 8, 30));
 Advogado advogado3 = new Advogado("Paulo Ricardo", new DateTime(1991, 3, 11));
-Cliente cliente1 = new Cliente("Rafaela", new DateTime(1991, 4, 11), "Solteiro", "Programador");
-Cliente cliente2 = new Cliente("Rubens", new DateTime(1992, 8, 22), "Casado", "Engenheiro");
-Cliente cliente3 = new Cliente("Ana", new DateTime(1997, 5, 23), "Solteiro", "Matemático");
+Cliente cliente1 = new Cliente("Rafaela", new DateTime(1991, 4, 11), "solteiro", "Programador");
+Cliente cliente2 = new Cliente("Rubens", new DateTime(1992, 8, 22), "casado", "Engenheiro");
+Cliente cliente3 = new Cliente("Ana", new DateTime(1997, 5, 23), "solteiro", "Matemático");
 
 advogado1.Cna = "852";
 advogado2.Cna = "741";
@@ -28,3 +28,4 @@ addPeople.addCliente(cliente2);
 addPeople.addCliente(cliente3);
 
 
+ListAndReports.reports();
